6ebbecec3c5cb62efc0427df27eb7120f6a2a64a
Nifazur Rahman <nifazurrahman@Nifazurs-MacBook-Pro.local>
2025-07-18 12:07:51 +0600

    add user model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -26,7 +26,7 @@ export interface IUser {
     phone?: string;
     picture?: string;
     address?: string;
-    isDeleted?: string;
+    isDeleted?: boolean;
     isActive?: IsActive;
     isVerified?: string;
     role: Role;
