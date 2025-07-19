bf129ece434db0915fe52e12f65d20e2cdb658b5
Nifazur Rahman <nifazurrahman@Nifazurs-MacBook-Pro.local>
2025-07-19 23:57:53 +0600

    add checkAuth middleware and seed super admin

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -28,7 +28,7 @@ export interface IUser {
     address?: string;
     isDeleted?: boolean;
     isActive?: IsActive;
-    isVerified?: string;
+    isVerified?: boolean;
     role: Role;
     auths: IAuthProvider[];
     bookings?: Types.ObjectId[];
